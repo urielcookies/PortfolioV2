f0c6d395500d6749eef5947e6a6979defe776463
Ever Uriel Garcia <urielcookies@outlook.com>
2025-05-30 20:00:23 -0600

    switched tailwind versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
+import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
-
-  integrations: [react()]
+  integrations: [tailwind(), react()]
 });
