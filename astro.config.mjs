1357901edef01535138b251b5f5c19c984f279a8
Ever Uriel Garcia <urielcookies@outlook.com>
2026-02-04 13:32:39 -0700

    Update site URL for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://urielcookies.github.io',
+  site: 'https://everscode.com',
   base: '/PortfolioV2/',
   integrations: [tailwind(), react()],
 });
+
