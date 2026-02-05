9cc51eb706522cc89f8209ea7971869f3b590219
Ever Uriel Garcia <urielcookies@outlook.com>
2026-02-05 11:38:11 -0700

    Set correct base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://everscode.com',
+  site: 'https://urielcookies.github.io',
+  base: '/PortfolioV2',
   output: 'static',
   integrations: [tailwind(), react()],
 });
